babfeb15187cdd41f1f91ee8aa550d653ccda059
umapathy1729 <umapathy428@gmail.com>
2026-06-02 18:58:38 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -77,3 +77,48 @@
 #     aws_iam_role_policy_attachment.node_3,
 #   ]
 # }
+
+
+
+
+# outputs.tf
+#-----------
+
+
+#output "jenkins_ip" {
+# value = aws_instance.jenkins.public_ip
+#}
+
+#output "devops_tools_ip" {
+# value = aws_instance.devops_tools.public_ip
+#}
+
+#output "s3_bucket" {
+# value = aws_s3_bucket.app_bucket.bucket
+#}
+
+#output "cloudfront_url" {
+# value = aws_cloudfront_distribution.cdn.domain_name
+#}
+
+#output "eks_endpoint" {
+# value = aws_eks_cluster.main.endpoint
+#}
+
+#output "eks_cluster_name" {
+#  value = aws_eks_cluster.main.name
+#}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
