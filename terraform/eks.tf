f9b0dc55c5b19b11bda63eef1e7dc922b0b9d44b
umapathy1729 <umapathy428@gmail.com>
2026-06-02 18:54:15 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,75 +1,79 @@
-resource "aws_iam_role" "eks_role" {
-  name = "eks-cluster-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
+# resource "aws_iam_role" "eks_role" {
+#   name = "eks-cluster-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect    = "Allow"
+#       Principal = { Service = "eks.amazonaws.com" }
+#       Action    = "sts:AssumeRole"
+#     }]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
+# resource "aws_iam_role_policy_attachment" "eks_policy" {
+#   role       = aws_iam_role.eks_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+# }
 
-# EKS Cluster
-resource "aws_eks_cluster" "main" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_role.arn
-  vpc_config {
-    subnet_ids = data.aws_subnets.default.ids
-  }
-  depends_on = [aws_iam_role_policy_attachment.eks_policy]
-}
+# # EKS Cluster
+# resource "aws_eks_cluster" "main" {
+#   name     = var.cluster_name
+#   role_arn = aws_iam_role.eks_role.arn
+#
+#   vpc_config {
+#     subnet_ids = data.aws_subnets.default.ids
+#   }
+#
+#   depends_on = [
+#     aws_iam_role_policy_attachment.eks_policy
+#   ]
+# }
 
-# Node Role
-resource "aws_iam_role" "node_role" {
-  name = "eks-node-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
+# # Node Role
+# resource "aws_iam_role" "node_role" {
+#   name = "eks-node-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect    = "Allow"
+#       Principal = { Service = "ec2.amazonaws.com" }
+#       Action    = "sts:AssumeRole"
+#     }]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "node_1" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
+# resource "aws_iam_role_policy_attachment" "node_1" {
+#   role       = aws_iam_role.node_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+# }
 
-resource "aws_iam_role_policy_attachment" "node_2" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
+# resource "aws_iam_role_policy_attachment" "node_2" {
+#   role       = aws_iam_role.node_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+# }
 
-resource "aws_iam_role_policy_attachment" "node_3" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
+# resource "aws_iam_role_policy_attachment" "node_3" {
+#   role       = aws_iam_role.node_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+# }
 
-# Worker Nodes
-resource "aws_eks_node_group" "workers" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "workers"
-  node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids      = data.aws_subnets.default.ids
-  instance_types  = [var.node_instance_type]
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.node_1,
-    aws_iam_role_policy_attachment.node_2,
-    aws_iam_role_policy_attachment.node_3,
-  ]
-}
+# # Worker Nodes
+# resource "aws_eks_node_group" "workers" {
+#   cluster_name    = aws_eks_cluster.main.name
+#   node_group_name = "workers"
+#   node_role_arn   = aws_iam_role.node_role.arn
+#   subnet_ids      = data.aws_subnets.default.ids
+#   instance_types  = [var.node_instance_type]
+#
+#   scaling_config {
+#     desired_size = 2
+#     max_size     = 3
+#     min_size     = 1
+#   }
+#
+#   depends_on = [
+#     aws_iam_role_policy_attachment.node_1,
+#     aws_iam_role_policy_attachment.node_2,
+#     aws_iam_role_policy_attachment.node_3,
+#   ]
+# }
