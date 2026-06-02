770b66e1265d1888992776a6d14d449ed4d8ce6f
umapathy1729 <umapathy428@gmail.com>
2026-06-02 19:04:54 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,13 +6,13 @@ variable "bucket_name" {
   default = "my-app-bucket-yourname-2026"
 }
 
-variable "cluster_name" {
-  default = "my-eks-cluster"
-}
+# variable "cluster_name" {
+#   default = "my-eks-cluster"
+# }
 
-variable "node_instance_type" {
-  default = "c7i-flex.large"
-}
+# variable "node_instance_type" {
+#   default = "c7i-flex.large"
+# }
 
 variable "key_name" {
   default = "ansible-key"
