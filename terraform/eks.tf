1610a8b76a2a9d4053373fa942e1712111059d13
umapathy1729 <umapathy428@gmail.com>
2026-06-02 19:04:31 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -111,6 +111,31 @@
 
 
 
+# variables.tf
+#-------------
+
+#variable "aws_region" {
+#  default = "ap-southeast-2"
+#}
+
+#variable "bucket_name" {
+#  default = "my-app-bucket-yourname-2026"
+#}
+
+# variable "cluster_name" {
+#   default = "my-eks-cluster"
+# }
+
+# variable "node_instance_type" {
+#   default = "c7i-flex.large"
+# }
+
+#variable "key_name" {
+#  default = "ansible-key"
+#}
+
+
+
 
 
 
